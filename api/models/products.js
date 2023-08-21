cd4a527af156377709da0649192bb859d2bfaf5c
taahirahismail <taahirah.i@gmail.com>
2023-08-21 12:00:11 +0200

    products functions done

---
+++ api/models/products.js
@@ -0,0 +1,70 @@
+const db = require("../config");
+
+class Products {
+  fetchProducts(req, res) {
+    const query = `SELECT prodID, prodName, quantity, amount, category, prodUrl FROM Products;`;
+
+    db.query(query, (err, results) => {
+      if (err) throw err;
+
+      res.json({
+        status: res.statusCode,
+        results,
+      });
+    });
+  }
+
+  fetchProduct(req, res) {
+    const query = `SELECT prodID, prodName, quantity, amount, category, prodUrl FROM Products WHERE prodID = ${req.params.id};`;
+
+    db.query(query, (err, result) => {
+      if (err) throw err;
+
+      res.json({
+        status: res.statusCode,
+        result,
+      });
+    });
+  }
+
+  addProduct(req, res) {
+    const query = `INSERT INTO Products SET ?;`;
+
+    db.query(query, req.body, (err) => {
+      if (err) throw err;
+
+      res.json({
+        status: res.statusCode,
+        message: "A new product has been added!",
+      });
+    });
+  }
+
+  updateProduct(req, res) {
+    const query = `UPDATE Products SET ? WHERE prodID = ?;`;
+
+    db.query(query, [req.body, req.params.id], (err) => {
+      if (err) throw err;
+
+      res.json({
+        status: res.statusCode,
+        message: "Product updated!",
+      });
+    });
+  }
+
+  deleteProduct(req, res) {
+    const query = `DELETE FROM Products WHERE prodID = ${req.params.id};`;
+
+    db.query(query, (err) => {
+      if (err) throw err;
+
+      res.json({
+        status: res.statusCode,
+        message: "Product removed!",
+      });
+    });
+  }
+}
+
+module.exports = Products;
